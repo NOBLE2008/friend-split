163c31d9d0f17d0bae6d99c096144182d249f2f7
Noble Benjamin <nobleben2008@gmail.com>
2024-07-06 18:41:16 +0100

    Added friend components

---
+++ src/components/Friend.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Friend = () => {
+  return (
+    
+  )
+}
+
+export default Friend
---
+++ src/components/FriendCon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FriendCon = () => {
+  return (
+    <div className="sidebar">
+        
+    </div>
+  )
+}
+
+export default FriendCon
