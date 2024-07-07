17d656a78426a35eb01338e57dbf5bdcfd6dfa54
Noble Benjamin <nobleben2008@gmail.com>
2024-07-07 14:58:00 +0100

    Implemented Logic for p tag

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 const Friend = ({ item, selectedFriends, setSelectedFriends }) => {
-  return <li>
-    <img src="/download.jpeg" alt="" />
-    <p>{item.name}</p>
-  </li>;
+  return (
+    <li>
+      <img src="/download.jpeg" alt="" />
+      <h3>{item.name}</h3>
+      {item.me == item.friend ? (
+        <p>You and Anthony are even</p>
+      ) : item.owe === "you" ? (
+        <p className="red">
+          You owe {item.name} £{item.me}
+        </p>
+      ) : (
+        <p className="green">
+          {item.name} owes you £{item.friend}
+        </p>
+      )}
+    </li>
+  );
 };
 
 export default Friend;
