db6138940436ce1aa84bd8f09e1c7bfbe50c861a
Noble Benjamin <nobleben2008@gmail.com>
2024-07-07 14:02:22 +0100

    Added a p tag for the frind's name

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Friend = ({ item, selectedFriends, setSelectedFriends }) => {
   return <li>
     <img src="/download.jpeg" alt="" />
+    <p>{item.name}</p>
   </li>;
 };
 
