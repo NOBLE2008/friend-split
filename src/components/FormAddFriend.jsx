2f18502a89e1bc423f70584eb5f2ea24825acf8d
Noble Benjamin <nobleben2008@gmail.com>
2024-07-07 18:28:02 +0100

    Added a handleSubmit function for Add Friend Form

--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -3,19 +3,26 @@ import Button from "./Button";
 
 const FormAddFriend = ({ onAddFriend }) => {
   const [newFriend, setNewFriend] = useState({
-    name: '',
+    name: "",
     me: 0,
     friend: 0,
-    owe: 'none',
+    owe: "none",
   });
   const handleChange = (e) => {
     // Add friend logic here
     setNewFriend((cur) => {
-        return {
-            ...cur,
-            [e.target.name]: e.target.value
-        }
-    })
+      return {
+        ...cur,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddFriend((cur) => {
+      return [...cur, { ...newFriend, id: Date.now() }];
+    });
   };
   return (
     <form className="form-add-friend">
