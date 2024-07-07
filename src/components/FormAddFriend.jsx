abe86a44fb73a0f036b103911e8eadfe05b66b5d
Noble Benjamin <nobleben2008@gmail.com>
2024-07-07 17:55:04 +0100

    Added UI for Add Friend Form

--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const FormAddFriend = () => {
   return (
-    
-  )
-}
+    <form className="form-add-friend">
+      <label htmlFor="name">👫 Friend Name</label>
+      <input type="text" id="name" />
+      <label htmlFor="image">🌄 Image URL</label>
+      <input type="text" id="image" />
+      <Button>Add</Button>
+    </form>
+  );
+};
 
-export default FormAddFriend
+export default FormAddFriend;
