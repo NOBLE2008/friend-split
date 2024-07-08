73f2de36a7840c21253655488f514c616b46b41f
Noble Benjamin <nobleben2008@gmail.com>
2024-07-08 18:51:22 +0100

    Added name attributes for the inputs

--- src/components/FormUpdateFriend.jsx
+++ src/components/FormUpdateFriend.jsx
@@ -5,11 +5,11 @@ const FormUpdateFriend = ({selectedFriends}) => {
     <form action="" className="form-split-bill">
       <h2>Split a bill with {selectedFriends.name}</h2>
       <label htmlFor="value">💰 Bill Value</label>
-      <input type="text" id="value" />
+      <input type="text" id="value" name='total'/>
       <label htmlFor="your-expense">🧍‍♀️ Your expense</label>
-      <input type="text" id="your-expense" />
+      <input type="text" id="your-expense" name='me'/>
       <label htmlFor="friend-expense">👫 {selectedFriends.name}'s </label>
-      <input type="text" id="friend-expense" />
+      <input type="text" id="friend-expense" name='friend'/>
       <label htmlFor="who-pays">🤑 Who is paying</label>
       <select name="who" id="">
         <option value="0">You</option>
