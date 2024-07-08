41b31626e3dda224e4bee7eff4316f5b613c366d
Noble Benjamin <nobleben2008@gmail.com>
2024-07-08 18:09:57 +0100

    Made h2 for the bill split

--- src/components/FormUpdateFriend.jsx
+++ src/components/FormUpdateFriend.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const FormUpdateFriend = () => {
+const FormUpdateFriend = ({selectedFriends}) => {
   return (
     <form action="" className="form-split-bill">
-      <h2>Split a bill with {}</h2>
+      <h2>Split a bill with {selectedFriends.name}</h2>
     </form>
   )
 }
