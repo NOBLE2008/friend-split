863c430ff0004a0caf4fc4cef6dac37bf8e58187
Noble Benjamin <nobleben2008@gmail.com>
2024-07-08 18:13:30 +0100

    Added label and input for Bill Value

--- src/components/FormUpdateFriend.jsx
+++ src/components/FormUpdateFriend.jsx
@@ -4,6 +4,8 @@ const FormUpdateFriend = ({selectedFriends}) => {
   return (
     <form action="" className="form-split-bill">
       <h2>Split a bill with {selectedFriends.name}</h2>
+      <label htmlFor="value">💰 Bill Value</label>
+      <input type="text" id="value" />
     </form>
   )
 }
