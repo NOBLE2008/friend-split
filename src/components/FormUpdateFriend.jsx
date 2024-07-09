b0253ac4c61d28f20ce44e0ed081b417ce98f51c
Noble Benjamin <nobleben2008@gmail.com>
2024-07-09 19:51:32 +0100

    Finished app functionalities

--- src/components/FormUpdateFriend.jsx
+++ src/components/FormUpdateFriend.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import Button from "./Button";
 
-const FormUpdateFriend = ({ selectedFriends, friends, setSelectedFriends, setFriends }) => {
+const FormUpdateFriend = ({
+  selectedFriends,
+  friends,
+  setSelectedFriends,
+  setFriends,
+}) => {
   const friendConfirm = friends.find(
     (friend) => friend.id === selectedFriends.id
   );
@@ -10,45 +15,74 @@ const FormUpdateFriend = ({ selectedFriends, friends, setSelectedFriends, setFri
   } else {
     const [formVal, setFormVal] = useState({
       id: friendConfirm.id,
-      total: 0,
+      total: friendConfirm.total,
       me: friendConfirm.me,
       friend: friendConfirm.friend,
-      who: 'me'
+      who: "me",
     });
     const handleChange = (e) => {
-      setFormVal(cur => {
+      setFormVal((cur) => {
         return {
-         ...cur,
-          [e.target.name]: e.target.value
+          ...cur,
+          [e.target.name]: e.target.value,
         };
-      })
-    }
+      });
+    };
 
     const handleSubmit = (e) => {
       e.preventDefault();
       setFriends((cur) => {
-        return {
-          ...cur,
-          me: formVal.me,
-          friend: formVal.friend,
-          owe: formVal.me == formVal.friend ? 'none': formVal.who == 'me' ? 'friend' : 'you'
-        }
+        return cur.map((it) => {
+          return it.id == formVal.id
+            ? {
+                ...it,
+                me: formVal.me,
+                total: formVal.total,
+                friend: Number(formVal.total)-Number(formVal.me),
+                owe:
+                  formVal.me == formVal.friend
+                    ? "none"
+                    : formVal.who == "me"
+                    ? "friend"
+                    : "you",
+              }
+            : it;
+        });
       });
 
       setSelectedFriends({
-        id: 1
-      })
-      
-    }
+        id: 1,
+      });
+    };
     return (
       <form action="" className="form-split-bill" onSubmit={handleSubmit}>
         <h2>Split a bill with {selectedFriends.name}</h2>
         <label htmlFor="value">💰 Bill Value</label>
-        <input type="number" id="value" name="total" value={formVal.total} onChange={handleChange}/>
+        <input
+          type="number"
+          id="value"
+          name="total"
+          value={formVal.total}
+          onChange={handleChange}
+        />
         <label htmlFor="your-expense">🧍‍♀️ Your expense</label>
-        <input type="number" id="your-expense" name="me" value={formVal.me} onChange={handleChange}/>
-        <label htmlFor="friend-expense">👫 {selectedFriends.name}'s expense </label>
-        <input type="number" id="friend-expense" disabled name="friend" value={(formVal.total) - formVal.me}/>
+        <input
+          type="number"
+          id="your-expense"
+          name="me"
+          value={formVal.me}
+          onChange={handleChange}
+        />
+        <label htmlFor="friend-expense">
+          👫 {selectedFriends.name}'s expense{" "}
+        </label>
+        <input
+          type="number"
+          id="friend-expense"
+          disabled
+          name="friend"
+          value={formVal.total - formVal.me}
+        />
         <label htmlFor="who-pays">🤑 Who is paying</label>
         <select name="who" id="" value={formVal.who} onChange={handleChange}>
           <option value="me">You</option>
@@ -58,7 +92,6 @@ const FormUpdateFriend = ({ selectedFriends, friends, setSelectedFriends, setFri
       </form>
     );
   }
-
 };
 
 export default FormUpdateFriend;
